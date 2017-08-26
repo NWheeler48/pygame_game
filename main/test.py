3e589616d9d17fae7b388b156588ac17febb0e92
afroninja713 <31371449+afroninja713@users.noreply.github.com>
2017-08-26 11:32:34 -0400

    Add files via upload

---
+++ main/test.py
@@ -0,0 +1,16 @@
+import pygame 
+def load_image(name, colorkey=None):
+	'''Loads an image and converts it to use in pygame'''
+	fullname = os.path.join("assets", name) 
+	image = pygame.image.load(fullname) 
+	image = image.convert_alpha() 
+	'pygame.transform.scale(image,(75,75))'#prob not needed 
+	if colorkey is not None:
+		if colorkey is -1:
+			colorkey = image.get_at((0,0))
+	return image, image.get_rect() 
+def load_sound(name):
+	'''Loads a ogg file for sound'''
+	fullname = os.path.join('assets', name)
+	sound = pygame.mixer.Sound(fullname)
+	return sound 
